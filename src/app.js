3feb507f3e44f29c6f063303b46f9816ea543693
Marco <marco.t3471@mail.com>
2025-11-28 09:20:25 -0500

    Railway

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("API funcionando correctamente 🚀");
+});
+
 // Rutas activas
 app.use("/api/admins", adminRoutes);
 app.use("/api/productos", productoRoutes);
